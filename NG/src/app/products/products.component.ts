688d23f591efff8f46cdfab7785b1ffa9ca34f2a
Jesper <jeppbengtsson_@hotmail.com>
2018-09-10 15:06:13 +0200

    timer added to products

--- NG/src/app/products/products.component.ts
+++ NG/src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild, HostListener, Host } from '@angular/core';
+import { Router } from '@angular/router';
+import { interval, Subscription } from 'rxjs';
 
 declare var jquery :any;
 declare var $ :any;
@@ -12,9 +14,27 @@ import 'hammerjs';
 })
 export class ProductsComponent implements OnInit {
  
-  constructor() { }
+  constructor(private router : Router) { }
+
+  countDown: Subscription;
+    timer = (60 * 30);
+
+
+    startTimer(time: number): Subscription {
+        return interval(time).subscribe(() => {
+            if(this.timer > 0) {
+                this.timer--;
+            }
+            if(this.timer === 0) {
+                this.router.navigate(['']);
+                this.countDown.unsubscribe();
+            }
+        });
+    }
 
   ngOnInit() {
+    this.countDown = this.startTimer(1000);
+
     $('#carousel').on('hidden.bs.modal', function () {
         $('.collapse').collapse('hide');
       });
