4beb3ca39d8a4e5d0b2ef01be86c3ae7a35e4251
Jesper <jeppbengtsson_@hotmail.com>
2018-09-13 16:33:36 +0200

    cachedata getters

--- NG/src/app/data.service.ts
+++ NG/src/app/data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 import { ISlide } from './islide';
 import { IBundle } from './ibundle';
 
@@ -19,5 +21,49 @@ export class DataService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  constructor() { }
+  constructor(private _http: HttpClient) {
+    this.baseUrl = 'http://localhost:8080/api/';
+  }
+
+  emitSortChange(str: string) {
+    this.SortEmmitter.emit(str);
+  }
+
+  // ------------- Slides
+ // Update the local slide array
+ pushGlobMovie(slides: ISlide[]) {
+   this.slides = slides;
+ }
+ getCachedSlides(): ISlide[] { return this.slides; }
+ getAllSlides(): Observable<ISlide[]> {
+   return this._http.get<ISlide[]>('http://localhost:8080/api/allslides');
+ }
+
+ // Update the remove slide db from local
+ postSlide(slide: ISlide) {
+   return this._http.post<string>('http://localhost:8080/api/postslide', slide, this.httpOptions);
+ }
+
+
+  // ------------- Bundles
+ // Update the local bundle array
+ pushGlobBundle(bundles: IBundle[]) {
+   this.bundles = bundles;
+ }
+ getCachedBundles(): IBundle[] { return this.bundles; }
+ getAllBundles(): Observable<IBundle[]> {
+   return this._http.get<IBundle[]>('http://localhost:8080/api/allbundle');
+ }
+
+ // Update the remove bundle db from local
+ postBundle(bundle: IBundle) {
+   return this._http.post<string>('http://localhost:8080/api/postbundle', bundle, this.httpOptions);
+ }
+  
+
+  cacheData() {
+    this._http.get<ISlide[]>('http://localhost:8080/api/allslides').subscribe( slides => { this.slides = slides; });
+    this._http.get<IBundle[]>('http://localhost:8080/api/allbundles').subscribe( bundles => { this.bundles = bundles; });
+  }
+
 }
